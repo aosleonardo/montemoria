4e9cec027c9f2c8a1e540f3f44c7753b15435dd3
Leonardo Oliveira <aosleonardo@gmail.com>
2025-08-09 16:15:32 -0300

    configdeploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/montemoria",
+  base:"/monte_moria",
   server: {
     host: "::",
     port: 8080,
